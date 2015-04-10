16a71f62f308a596302b065ef2f1748ec6f4c5e8
xw.chen <happyfish1989@gmail.com>
2015-04-10 13:43:34 +0800

    completely demo code

--- python/crawler/crawler_demon.py
+++ python/crawler/crawler_demon.py
@@ -1,3 +1,9 @@
+import sys;
+import os;
+import re;
+import urllib2;
+
+
 def downURL( url, filename):
 	try:
 		fp = urllib2.urlopen(url);
@@ -17,3 +23,59 @@ def downURL( url, filename):
 	fp.close();
 	op.close();
 	return 1;
+
+def getURL(url):
+	try:
+		fp = urllib2.urlopen(url)
+	except:
+		print 'get url exception'
+		return []
+	pass
+
+	pattern = re.compile("http://sports.sina.com.cn/[^\>]+.shtml")
+	while 1:
+		s = fp.read();
+		if not s:
+			break;
+			pass
+		urls = pattern.findall(s);
+		pass
+	fp.close();
+	return urls;
+
+def spider(startURL, times):
+	urls = []
+	urls.append(startURL);
+	i = 0;
+	while 1:
+		if i > times:
+			break;
+			pass
+		if len(urls) > 0:
+			url = urls.pop(0);
+			print url, len(urls);
+			downURL(url, str(i)+".htm");
+			i = i+1;
+			if len(urls) < times:
+				urllist = getURL(url)
+				for url in urllist:
+					if urls.count(url)==0:
+						urls.append(url);
+						pass
+				 	pass 
+				pass
+			pass
+		else:
+			break;
+			pass;
+		pass
+	pass
+	return 1;
+
+
+def main():
+	print 'hello, crawler world!'
+	pass
+
+if __name__ == '__main__':
+	main()
